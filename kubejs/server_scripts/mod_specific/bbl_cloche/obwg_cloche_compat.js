4cb8a47be1aa583c11e3c1ffb3b3d7fb532bf098
Corsta1994 <corey.swannell@gmail.com>
2025-03-18 22:07:51 +1030

    finished cloche compat files

---
+++ kubejs/server_scripts/mod_specific/bbl_cloche/obwg_cloche_compat.js
@@ -0,0 +1,121 @@
+ServerEvents.recipes(event => {
+    //tree('biomeswevegone:_sapling', 2, 'biomeswevegone:_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:_log', 0.2, 1, 'biomeswevegone:_sapling', chance_3, output_amount_3, output_3, chance_4, output_amount_4, output_4, chance_5, output_amount_5, output_5)
+
+    tree('biomeswevegone:araucaria_sapling', 2, 'biomeswevegone:araucaria_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:pine_log', 0.2, 1, 'biomeswevegone:araucaria_sapling')
+    tree('biomeswevegone:aspen_sapling', 2, 'biomeswevegone:aspen_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:aspen_log', 0.2, 1, 'biomeswevegone:aspen_sapling', 0.1, 1, 'biomeswevegone:shelf_fungi')
+    tree('biomeswevegone:baobab_sapling', 2, 'biomeswevegone:baobab_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:baobab_log', 0.2, 1, 'biomeswevegone:baobab_sapling', 0.1, 1, 'biomeswevegone:flowering_baobab_leaves', 0.1, 1, 'biomeswevegone:baobab_fruit')
+    tree('biomeswevegone:blue_enchanted_sapling', 2, 'biomeswevegone:blue_enchanted_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:blue_enchanted_log', 0.2, 1, 'biomeswevegone:blue_enchanted_sapling')
+    tree('biomeswevegone:blue_spruce_sapling', 2, 'biomeswevegone:blue_spruce_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:spruce_log', 0.2, 1, 'biomeswevegone:blue_spruce_sapling')
+    tree('biomeswevegone:brown_birch_sapling', 2, 'biomeswevegone:brown_birch_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:birch_log', 0.2, 1, 'biomeswevegone:brown_birch_sapling')
+    tree('biomeswevegone:brown_zelkova_sapling', 2, 'biomeswevegone:brown_zelkova_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:zelkova_log', 0.2, 1, 'biomeswevegone:brown_zelkova_sapling')
+    tree('biomeswevegone:cika_sapling', 2, 'biomeswevegone:cika_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:cika_log', 0.2, 1, 'biomeswevegone:cika_sapling')
+    tree('biomeswevegone:cypress_sapling', 2, 'biomeswevegone:cypress_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:cypress_log', 0.2, 1, 'biomeswevegone:cypress_sapling', 0.1, 1, 'minecraft:moss_carpet', 0.1, 1, 'minecraft:vine')
+    tree('biomeswevegone:ebony_sapling', 2, 'biomeswevegone:ebony_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:ebony_log', 0.2, 1, 'biomeswevegone:ebony_sapling')
+    tree('biomeswevegone:fir_sapling', 2, 'biomeswevegone:fir_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:fir_log', 0.2, 1, 'biomeswevegone:fir_sapling')
+    tree('biomeswevegone:green_enchanted_sapling', 2, 'biomeswevegone:green_enchanted_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:green_enchanted_log', 0.2, 1, 'biomeswevegone:green_enchanted_sapling')
+    tree('biomeswevegone:holly_sapling', 2, 'biomeswevegone:holly_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:holly_log', 0.2, 1, 'biomeswevegone:holly_sapling', 0.1, 1, 'biomeswevegone:holly_berry_leaves', 0.1, 1, 'minecraft:sweet_berries')
+    tree('biomeswevegone:indigo_jacaranda_sapling', 2, 'biomeswevegone:indigo_jacaranda_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:jacaranda_log', 0.2, 1, 'biomeswevegone:indigo_jacaranda_sapling', 0.1, 1, 'biomeswevegone:flowering_indigo_jacaranda_leaves')
+    tree('biomeswevegone:ironwood_sapling', 2, 'biomeswevegone:ironwood_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:ironwood_log', 0.2, 1, 'biomeswevegone:ironwood_sapling')
+    tree('biomeswevegone:jacaranda_sapling', 2, 'biomeswevegone:jacaranda_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:jacaranda_log', 0.2, 1, 'biomeswevegone:jacaranda_sapling', 0.1, 1, 'biomeswevegone:flowering_jacaranda_leaves')
+    tree('biomeswevegone:mahogany_sapling', 2, 'biomeswevegone:mahogany_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:mahogany_log', 0.2, 1, 'biomeswevegone:mahogany_sapling', 0.1, 1, 'minecraft:vine')
+    tree('biomeswevegone:maple_sapling', 2, 'biomeswevegone:maple_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:maple_log', 0.2, 1, 'biomeswevegone:maple_sapling')
+    tree('biomeswevegone:orange_birch_sapling', 2, 'biomeswevegone:orange_birch_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:birch_log', 0.2, 1, 'biomeswevegone:orange_birch_sapling')
+    tree('biomeswevegone:orange_oak_sapling', 2, 'biomeswevegone:orange_oak_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:oak_log', 0.2, 1, 'biomeswevegone:orange_oak_sapling')
+    tree('biomeswevegone:orange_spruce_sapling', 2, 'biomeswevegone:orange_spruce_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:spruce_log', 0.2, 1, 'biomeswevegone:orange_spruce_sapling')
+    tree('biomeswevegone:orchard_sapling', 2, 'biomeswevegone:orchard_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:oak_log', 0.2, 1, 'biomeswevegone:orchard_sapling', 0.1, 1, 'biomeswevegone:flowering_orchard_leaves', 0.1, 1, 'biomeswevegone:ripe_orchard_leaves', 0.1, 1, 'minecraft:apple')
+    tree('biomeswevegone:palm_sapling', 2, 'biomeswevegone:palm_leaves', 'floabg:farmland_sand', 1200, 1.0, 1, 'biomeswevegone:palm_log', 0.2, 1, 'biomeswevegone:palm_sapling')
+    tree('biomeswevegone:palo_verde_sapling', 2, 'biomeswevegone:palo_verde_leaves', 'floabg:farmland_sand', 1200, 1.0, 1, 'biomeswevegone:palo_verde_log', 0.2, 1, 'biomeswevegone:palo_verde_sapling')
+    tree('biomeswevegone:pine_sapling', 2, 'biomeswevegone:pine_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:pine_log', 0.2, 1, 'biomeswevegone:pine_sapling')
+    tree('biomeswevegone:rainbow_eucalyptus_sapling', 2, 'biomeswevegone:rainbow_eucalyptus_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:rainbow_eucalyptus_log', 0.2, 1, 'biomeswevegone:rainbow_eucalyptus_sapling', 0.1, 1, 'minecraft:vine')
+    tree('biomeswevegone:red_birch_sapling', 2, 'biomeswevegone:red_birch_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:birch_log', 0.2, 1, 'biomeswevegone:red_birch_sapling')
+    tree('biomeswevegone:red_oak_sapling', 2, 'biomeswevegone:red_oak_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:oak_log', 0.2, 1, 'biomeswevegone:red_oak_sapling')
+    tree('biomeswevegone:red_spruce_sapling', 2, 'biomeswevegone:red_spruce_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:spruce_log', 0.2, 1, 'biomeswevegone:red_spruce_sapling')
+    tree('biomeswevegone:redwood_sapling', 2, 'biomeswevegone:redwood_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:redwood_log', 0.2, 1, 'biomeswevegone:redwood_sapling')
+    tree('biomeswevegone:silver_maple_sapling', 2, 'biomeswevegone:silver_maple_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:maple_log', 0.2, 1, 'biomeswevegone:silver_maple_sapling')
+    tree('biomeswevegone:skyris_sapling', 2, 'biomeswevegone:skyris_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:skyris_log', 0.2, 1, 'biomeswevegone:skyris_sapling', 0.1, 1, 'biomeswevegone:green_apple_skyris_leaves', 0.1, 1, 'biomeswevegone:flowering_skyris_leaves', 0.1, 1, 'biomeswevegone:skyris_vine')
+    tree('biomeswevegone:spirit_sapling', 2, 'biomeswevegone:spirit_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:spirit_log', 0.2, 1, 'biomeswevegone:spirit_sapling', 0.1, 1, 'biomeswevegone:spirit_roots', 0.1, 1, 'biomeswevegone:flowering_spirit_leaves')
+    tree('biomeswevegone:white_mangrove_sapling', 2, 'biomeswevegone:white_mangrove_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:white_mangrove_log', 0.2, 1, 'biomeswevegone:white_mangrove_sapling', 0.1, 1, 'minecraft:vine')
+    tree('biomeswevegone:white_sakura_sapling', 2, 'biomeswevegone:white_sakura_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:sakura_log', 0.2, 1, 'biomeswevegone:white_sakura_sapling')
+    tree('biomeswevegone:willow_sapling', 2, 'biomeswevegone:willow_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:willow_log', 0.2, 1, 'biomeswevegone:willow_sapling', 0.1, 1, 'minecraft:moss_carpet', 0.1, 1, 'minecraft:vine')
+    tree('biomeswevegone:witch_hazel_sapling', 2, 'biomeswevegone:witch_hazel_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:witch_hazel_log', 0.2, 1, 'biomeswevegone:witch_hazel_sapling', 0.1, 1, 'biomeswevegone:blooming_witch_hazel_leaves', 0.1, 1, 'biomeswevegone:witch_hazel_blossom', 0.1, 1, 'biomeswevegone:witch_hazel_branch')
+    tree('biomeswevegone:yellow_birch_sapling', 2, 'biomeswevegone:yellow_birch_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:birch_log', 0.2, 1, 'biomeswevegone:yellow_birch_sapling')
+    tree('biomeswevegone:yellow_sakura_sapling', 2, 'biomeswevegone:yellow_sakura_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:sakura_log', 0.2, 1, 'biomeswevegone:yellow_sakura_sapling')
+    tree('biomeswevegone:yellow_spruce_sapling', 2, 'biomeswevegone:yellow_spruce_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'minecraft:spruce_log', 0.2, 1, 'biomeswevegone:yellow_spruce_sapling')
+    tree('biomeswevegone:yucca_sapling', 2, 'biomeswevegone:yucca_leaves', 'floabg:farmland_sand', 1200, 1.0, 1, 'minecraft:oak_log', 0.2, 1, 'biomeswevegone:yucca_sapling', 0.1, 1, 'biomeswevegone:flowering_yucca_leaves', 0.1, 1, 'biomeswevegone:ripe_yucca_leaves')
+    tree('biomeswevegone:zelkova_sapling', 2, 'biomeswevegone:zelkova_leaves', 'floabg:farmland_grass', 1200, 1.0, 1, 'biomeswevegone:zelkova_log', 0.2, 1, 'biomeswevegone:zelkova_sapling')
+
+function tree(sapling, leaf_amount, leaf, farmland_tag, duration, chance_1, output_amount_1, output_1, chance_2, output_amount_2, output_2, chance_3, output_amount_3, output_3, chance_4, output_amount_4, output_4, chance_5, output_amount_5, output_5) {
+
+    if (chance_2 === undefined) {
+      chance_2 = 0.0;
+      output_amount_2 = 0;
+      output_2 = "kubejs:invisible_item"
+    };
+    if (chance_3 === undefined) {
+      chance_3 = 0.0;
+      output_amount_3 = 0;
+      output_3 = "kubejs:invisible_item"
+    };
+    if (chance_4 === undefined) {
+      chance_4 = 0.0;
+      output_amount_4 = 0;
+      output_4 = "kubejs:invisible_item"
+    };
+    if (chance_5 === undefined) {
+      chance_5 = 0.0;
+      output_amount_5 = 0;
+      output_5 = "kubejs:invisible_item"
+    };
+    event.custom({
+      "type": "cloche:cloche",
+      "catalyst": [],
+      "duration": duration,
+      "results": [
+          {
+            "chance": chance_1,
+            "item": {                
+              "count": output_amount_1,
+              "id": output_1
+            }
+          },
+        {
+          "chance": chance_2,
+          "item": {
+            "count": output_amount_2,
+            "id": output_2
+          }
+        },
+        {
+          "chance": chance_3,
+          "item": {
+            "count": output_amount_3,
+            "id": output_3
+          }
+        },
+        {
+          "chance": chance_4,
+          "item": {
+            "count": output_amount_4,
+            "id": output_4
+          }
+        },
+        {
+          "chance": chance_5,
+          "item": {
+            "count": output_amount_5,
+            "id": output_5
+          }
+        }
+      ],
+      "seed": {
+        "item": sapling
+      },
+      "shears_result": {
+        "count": leaf_amount,
+        "id": leaf
+      },
+      "soil": {
+        "tag": farmland_tag
+      }
+    })}
+})
--- kubejs/server_scripts/mod_specific/bbl_cloche/silentgear_cloche_compat.js
+++ kubejs/server_scripts/mod_specific/bbl_cloche/silentgear_cloche_compat.js
@@ -1,43 +1,7 @@
 ServerEvents.recipes(event => {
-    event.custom({
-      "type": "cloche:cloche",
-      "catalyst": [],
-      "duration": 1200,
-      "results": [
-        {
-          "item": {
-            "count": 2,
-            "id": "silentgear:netherwood_log"
-          }
-        },
-        {
-          "chance": 0.2,
-          "item": {
-            "count": 1,
-            "id": "silentgear:netherwood_sapling"
-          }
-        },
-        {
-          "chance": 0.1,
-          "item": {
-            "count": 1,
-            "id": "silentgear:nether_banana"
-          }
-        }
-      ],
-      "seed": {
-        "item": "silentgear:netherwood_sapling"
-      },
-      "shears_result": {
-        "count": 2,
-        "id": "silentgear:netherwood_leaves"
-      },
-      "soil": {
-        "tag": "floabg:farmland_grass"
-      }
-    })
-    crop('silentgear:fluffy_seeds', farmland_tag, 1200, 1.0, 1, 'silentgear:fluffy_puff')
-    crop('silentgear:flax_seeds', farmland_tag, 1200, 1.0, 1, 'silentgear:flax_fiber', 0.1, 1, 'silentgear:flax_flowers')
+    crop('silentgear:fluffy_seeds', 'floabg:farmland_0', 1200, 1.0, 1, 'silentgear:fluffy_puff')
+    crop('silentgear:flax_seeds', 'floabg:farmland_0', 1200, 1.0, 1, 'silentgear:flax_fiber', 0.1, 1, 'silentgear:flax_flowers') 
+    tree("silentgear:netherwood_sapling", 2, "silentgear:netherwood_leaves", "floabg:farmland_grass", 1200, 1.0, 2, "silentgear:netherwood_log", 0.2, 1, "silentgear:netherwood_sapling", 0.1, 1, "silentgear:nether_banana")
 
     function crop( seed, farmland_tag, duration, chance, output_amount, output, chance_1, output_amount_1, output_1) {
 
@@ -74,4 +38,57 @@ ServerEvents.recipes(event => {
         "tag": farmland_tag
       }
     })}  
+
+   
+
+    function tree(sapling, leaf_amount, leaf, farmland_tag, duration, chance_1, output_amount_1, output_1, chance_2, output_amount_2, output_2, chance_3, output_amount_3, output_3) {
+
+      if (chance_2 === undefined) {
+        chance_2 = 0.0;
+        output_amount_2 = 0;
+        output_2 = "kubejs:invisible_item"
+      };
+      if (chance_3 === undefined) {
+        chance_3 = 0.0;
+        output_amount_3 = 0;
+        output_3 = "kubejs:invisible_item"
+      };
+      event.custom({
+        "type": "cloche:cloche",
+        "catalyst": [],
+        "duration": duration,
+        "results": [
+            {
+              "chance": chance_1,
+              "item": {                
+                "count": output_amount_1,
+                "id": output_1
+              }
+            },
+          {
+            "chance": chance_2,
+            "item": {
+              "count": output_amount_2,
+              "id": output_2
+            }
+          },
+          {
+            "chance": chance_3,
+            "item": {
+              "count": output_amount_3,
+              "id": output_3
+            }
+          }
+        ],
+        "seed": {
+          "item": sapling
+        },
+        "shears_result": {
+          "count": leaf_amount,
+          "id": leaf
+        },
+        "soil": {
+          "tag": farmland_tag
+        }
+      })} 
 })
