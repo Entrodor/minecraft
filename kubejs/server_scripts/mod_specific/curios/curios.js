516137435f51a52d1fda78f2992200f1e2480f84
Entrodor <lordmegaminer2@gmail.com>
2025-04-06 18:39:38 +1000

    simplemagnets changes

--- kubejs/server_scripts/mod_specific/curios/curios.js
+++ kubejs/server_scripts/mod_specific/curios/curios.js
@@ -1,7 +1,14 @@
 ServerEvents.tags('item', event => {
+// Add curios tags to items
     event.add('curios:body', 'mekanism:jetpack')
     event.add('curios:body', 'mekanism:jetpack_armored')
     event.add('curios:necklace', 'forbidden_arcanus:spectral_eye_amulet')
     event.add('curios:quiver', 'supplementaries:quiver')
     event.add('curios:curio', 'justdirethings:pocket_generator')
+    event.add('curios:magnet', 'simplemagnets:basicmagnet')
+    event.add("curios:magnet", "simplemagnets:advancedmagnet")
+
+// Remove curios tags from items
+    event.remove("curios:charm", "simplemagnets:advancedmagnet")
+    event.remove("curios:charm", "simplemagnets:basicmagnet")
 })
