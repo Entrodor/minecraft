774d1b886b68e5c9219341af0ded28eafbc24dfb
Corsta1994 <corey.swannell@gmail.com>
2025-04-11 22:50:09 +0930

    worked on quests

--- kubejs/server_scripts/mod_specific/mystical_agriculture/mystical_essence_recipes.js
+++ kubejs/server_scripts/mod_specific/mystical_agriculture/mystical_essence_recipes.js
@@ -1,4 +1,8 @@
 ServerEvents.recipes(event => {
+  const essence = ["iron", "gold", "copper", "uranium", "tin", "aluminum", "lead", "nickel", "platinum", "osmium", "silver", "zinc", "iridium"]
+  essence.forEach(ingred => {
+    event.remove({input: `mysticalagriculture:${ingred}_essence`})
+  })
   //Mystical
   event.shaped('mysticalagriculture:prosperity_shard', ['AAA', 'AAA', 'AAA'], {
     A: 'mysticalagriculture:prosperity_shard_essence'
@@ -18,6 +22,46 @@ ServerEvents.recipes(event => {
 
   event.shaped('allthemodium:unobtainium_nugget', ['AAA', 'A A', 'AAA'], {
     A: 'mysticalagriculture:unobtainium_essence'
+  })
+  //All The Ores
+  event.shaped('6x minecraft:raw_iron', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:iron_essence'
+  })
+  event.shaped('6x minecraft:raw_copper', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:copper_essence'
+  })
+  event.shaped('4x minecraft:raw_gold', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:gold_essence'
+  })
+  event.shaped('6x alltheores:raw_aluminum', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:aluminum_essence'
+  })
+  event.shaped('4x alltheores:raw_lead', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:lead_essence'
+  })
+  event.shaped('4x alltheores:raw_nickel', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:nickel_essence'
+  })
+  event.shaped('2x alltheores:raw_platinum', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:platinum_essence'
+  })
+  event.shaped('4x alltheores:raw_osmium', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:osmium_essence'
+  })
+  event.shaped('4x alltheores:raw_silver', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:silver_essence'
+  })
+  event.shaped('4x alltheores:raw_tin', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:tin_essence'
+  })
+  event.shaped('4x alltheores:raw_zinc', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:zinc_essence'
+  })
+  event.shaped('2x alltheores:raw_iridium', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:iridium_essence'
+  })
+  event.shaped('2x alltheores:raw_uranium', ['AAA', 'A A', 'AAA'], {
+    A: 'mysticalagriculture:uranium_essence'
   })
 
   //Forbidden & Arcanus
